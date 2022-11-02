c2b30ec723880c10ba19bc989d2a9bc56ebddd55
KrisN9 <kristinank992@gmail.com>
2022-11-02 20:28:45 +0000

    Inicio Landing Page

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -6,7 +6,7 @@ const MyCard = () => {
           <img className="card-img-top" src="..." alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
             <a href="#" className="btn btn-primary">Find out more!</a>
           </div>
         </div>

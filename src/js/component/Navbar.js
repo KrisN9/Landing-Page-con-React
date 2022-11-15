42b4718999270c154038b0e4e2af97cb643b6a00
KrisN9 <kristinank992@gmail.com>
2022-11-15 01:17:41 +0000

    Ultimado para entrega

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-md bg-dark">
+        <nav className="navbar navbar-expand-md bg-black">
             <div className="container">
                 <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"><i className="bi bi-list"></i></span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <div className="navbar-nav">
-                        <a className="nav-link active text-muted" id="nav-link" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active text-white" id="nav-link" aria-current="page" href="#">Home</a>
                         <a className="nav-link active text-muted" id="nav-link" href="#">About</a>
                         <a className="nav-link active text-muted" id="nav-link" href="#">Services</a>
                         <a className="nav-link active text-muted" id="nav-link" href="#">Contact</a>
